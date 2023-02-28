d0783b62a0a4b77d1c8178d4effb257dedf6b649
alexveryk <veryk.av@gmail.com>
2023-03-01 00:43:08 +0200

    Update ContactList.jsx

--- src/components/ContactLst/ContactList.jsx
+++ src/components/ContactLst/ContactList.jsx
@@ -1,14 +1,24 @@
+import PropTypes from 'prop-types';
+import { List } from './ContactList.styled';
+import { ListItem } from './ContactList.styled';
+import { BtnDelete } from './ContactList.styled';
+
 export const ContactLst = ({ visibleName, onDelete }) => {
   return (
-    <ul>
+    <List>
       {visibleName.map(contact => {
         return (
-          <li key={contact.id}>
+          <ListItem key={contact.id}>
             {contact.name}: {contact.number}
-            <button onClick={() => onDelete(contact.id)}> Delete</button>
-          </li>
+            <BtnDelete onClick={() => onDelete(contact.id)}> Delete</BtnDelete>
+          </ListItem>
         );
       })}
-    </ul>
+    </List>
   );
+};
+
+ContactLst.prototype = {
+  visibleName: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  onDelete: PropTypes.func.isRequired,
 };
