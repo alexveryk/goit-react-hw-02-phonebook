a3133765113fb5b3b4497df83eac23ec96cfea88
alexveryk <veryk.av@gmail.com>
2023-02-28 22:07:15 +0200

    Update ContactList.jsx

--- src/components/ContactLst/ContactList.jsx
+++ src/components/ContactLst/ContactList.jsx
@@ -1,10 +1,11 @@
-export const ContactLst = ({ visibleName }) => {
+export const ContactLst = ({ visibleName, onDelete }) => {
   return (
     <ul>
       {visibleName.map(contact => {
         return (
           <li key={contact.id}>
             {contact.name}: {contact.number}
+            <button onClick={() => onDelete(contact.id)}> Delete</button>
           </li>
         );
       })}
